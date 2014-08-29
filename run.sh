d992e720c866b7d97c425a2b01443f3b6ee0a9e2
Gabor Szarnyas <szarnyasg@gmail.com>
2014-08-29 19:38:24 +0200

    Added ps command.

--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-4s-backend-setup -v tb_cluster
-4s-backend tb_cluster
+CLUSTERNAME=tb_cluster
 
-4s-import tb_cluster railway-xform-1.ttl --format turtle
-4s-query tb_cluster "SELECT ?x ?y ?z WHERE { ?x ?y ?z }" -f text | head -n 20
+4s-backend-setup -v $CLUSTERNAME
+4s-backend $CLUSTERNAME
+
+ps auxw | grep 4s-backen[d]
+
+4s-import $CLUSTERNAME railway-xform-1.ttl --format turtle
+4s-query $CLUSTERNAME "SELECT ?x ?y ?z WHERE { ?x ?y ?z }" -f text | head -n 20
 
