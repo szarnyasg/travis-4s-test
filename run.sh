52e1600eb705c3865ab97d3856a1cb5a764b965e
Gabor Szarnyas <szarnyasg@gmail.com>
2014-08-29 19:28:20 +0200

    Added verbose mode.

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-4s-backend-setup tb_cluster
-4s-backend tb_cluster
+4s-backend-setup -v tb_cluster
+4s-backend -v tb_cluster
 
 4s-import tb_cluster railway-xform-1.ttl --format turtle
 4s-query tb_cluster "SELECT ?x ?y ?z WHERE { ?x ?y ?z }" -f text | head -n 20
