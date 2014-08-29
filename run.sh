7b74812dc30fbaa60f6628df67b07367934fbf3e
Gabor Szarnyas <szarnyasg@gmail.com>
2014-08-29 20:03:59 +0200

    Back to single mode.

--- run.sh
+++ run.sh
@@ -2,10 +2,12 @@
 
 CLUSTERNAME=tb_cluster
 
-4s-cluster-create $CLUSTERNAME
-4s-cluster-start $CLUSTERNAME
+4s-backend-setup -v $CLUSTERNAME
+4s-backend $CLUSTERNAME
 
 ps auxw | grep 4s-backen[d]
+ls -la /var/lib/4store
+ls -la /var/lib/4store/$CLUSTERNAME
 
 4s-import $CLUSTERNAME railway-xform-1.ttl --format turtle
 4s-query $CLUSTERNAME "SELECT ?x ?y ?z WHERE { ?x ?y ?z }" -f text | head -n 20
