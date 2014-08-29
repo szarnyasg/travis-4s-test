b6beecc37c93040cbcc2e16cbfb1df50f2839c47
Gabor Szarnyas <szarnyasg@gmail.com>
2014-08-29 19:15:45 +0200

    changed clustername.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-4s-backend-setup my_cluster
-4s-backend my_cluster
+4s-backend-setup trainbenchmark_cluster
+4s-backend trainbenchmark_cluster
 
-4s-import my_cluster railway-xform-1.ttl --format turtle
+4s-import trainbenchmark_cluster railway-xform-1.ttl --format turtle
