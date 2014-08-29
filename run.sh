9d47975cbb7abb4c8d12aab0fabe8fca4c8035de
Gabor Szarnyas <szarnyasg@gmail.com>
2014-08-29 19:18:07 +0200

    Shortened cluster name.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-4s-backend-setup trainbenchmark_cluster
-4s-backend trainbenchmark_cluster
+4s-backend-setup tb_cluster
+4s-backend tb_cluster
 
-4s-import trainbenchmark_cluster railway-xform-1.ttl --format turtle
+4s-import tb_cluster railway-xform-1.ttl --format turtle
