c735d623a44f4b107f6c5e3ea121d96b8e0169bc
Gabor Szarnyas <szarnyasg@gmail.com>
2014-08-30 15:09:42 +0200

    Redirected stderr to stdout.

--- run.sh
+++ run.sh
@@ -9,8 +9,8 @@ cat /etc/avahi/avahi-daemon.conf | grep use-ipv6
 sudo iptables -F
 
 # initialize 4store
-4s-backend-setup -v $CLUSTERNAME
-4s-backend $CLUSTERNAME
+4s-backend-setup -v $CLUSTERNAME 2>&1
+4s-backend $CLUSTERNAME 2>&1
 sleep 5
 
 # check if 4store runs
@@ -18,7 +18,7 @@ ps auxw | grep 4s-backen[d]
 ls -la /var/lib/4store/$CLUSTERNAME
 
 # import and query
-4s-import $CLUSTERNAME railway-xform-1.ttl --format turtle -v -v
-4s-query $CLUSTERNAME "SELECT ?x ?y ?z WHERE { ?x ?y ?z }" -f text --verbose | head -n 20
+4s-import $CLUSTERNAME railway-xform-1.ttl --format turtle -v -v 2>&1
+4s-query $CLUSTERNAME "SELECT ?x ?y ?z WHERE { ?x ?y ?z }" -f text --verbose | head -n 20 2>&1
 
 sudo cat /var/log/syslog
