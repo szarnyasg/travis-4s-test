d6af2b120927593d083c46787b7c33317c311b51
Gabor Szarnyas <szarnyasg@gmail.com>
2014-08-30 20:36:27 +0200

    added config file to other 4s calls.

--- run.sh
+++ run.sh
@@ -22,5 +22,5 @@ sleep 1
 4s-admin --config-file 4store.conf list-stores
 4s-admin --config-file 4store.conf start-stores my_repository
 sleep 1
-4s-import -v my_repository railway-xform-1.ttl --format turtle
-4s-query my_repository "SELECT ?x ?y ?z WHERE { ?x ?y ?z }" -f text --verbose | head -n 20
+4s-import --config-file 4store.conf -v my_repository railway-xform-1.ttl --format turtle
+4s-query --config-file 4store.conf my_repository "SELECT ?x ?y ?z WHERE { ?x ?y ?z }" -f text --verbose | head -n 20
