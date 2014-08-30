14b8289c96f56b593d84535fb588d613a38bfe06
Gabor Szarnyas <szarnyasg@gmail.com>
2014-08-30 17:23:58 +0200

    Simplified script.

--- run.sh
+++ run.sh
@@ -2,6 +2,10 @@
 
 CLUSTERNAME=my_cluster
 
+pkill -f 4s
+sleep 1
+rm -rf /var/lib/4store/$CLUSTERNAME
+
 sudo touch /etc/4store.conf
 sudo chown $USER:$USER /etc/4store.conf
 echo '[4s-boss]
@@ -11,16 +15,18 @@ nodes = localhost
 [my_cluster]
 port = 7890' > /etc/4store.conf
 
+
 cat /etc/4store.conf
 
 4s-boss
 4s-admin create-store --segments=2 $CLUSTERNAME
+4s-admin list-nodes
 4s-admin list-stores
 4s-admin start-stores $CLUSTERNAME
 
 # import and query
 4s-import $CLUSTERNAME railway-xform-1.ttl --format turtle -v -v 2>&1
-4s-query $CLUSTERNAME "SELECT ?x ?y ?z WHERE { ?x ?y ?z }" -f text --verbose | head -n 20 2>&1
+#4s-query $CLUSTERNAME "SELECT ?x ?y ?z WHERE { ?x ?y ?z }" -f text --verbose | head -n 20 2>&1
 
 #ps auxw | grep avah[i]
 #service avahi-daemon status
