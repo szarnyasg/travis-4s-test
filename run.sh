63402294f08bd34e428cc4e7f2b3bebf6fac0a74
Gabor Szarnyas <szarnyasg@gmail.com>
2014-08-30 17:06:52 +0200

    Trying 4s-boss.

--- run.sh
+++ run.sh
@@ -20,12 +20,10 @@ ls -la /var/lib/4store/$CLUSTERNAME
 4s-import $CLUSTERNAME railway-xform-1.ttl --format turtle -v -v 2>&1
 4s-query $CLUSTERNAME "SELECT ?x ?y ?z WHERE { ?x ?y ?z }" -f text --verbose | head -n 20 2>&1
 
-sudo touch /etc/4store.conf
-
 #ps auxw | grep avah[i]
 #service avahi-daemon status
 
 #avahi-browse _4store._tcp
 #avahi-browse --all --terminate
 
-#sudo cat /var/log/syslog
+sudo cat /var/log/syslog
